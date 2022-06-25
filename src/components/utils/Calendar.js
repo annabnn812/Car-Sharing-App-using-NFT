d1585c91bbb013eb2165be535b113031e6443c71
Anna <86491435+annabnn812@users.noreply.github.com>
2022-06-25 01:35:58 -0400

    Add files via upload

---
+++ src/components/utils/Calendar.js
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+import ReactLightCalendar from "@lls/react-light-calendar";
+import "@lls/react-light-calendar/dist/index.css";
+
+const DAY_LABELS = [
+  'Monday', 
+  'Tuesday', 
+  'Wednesday', 
+  'Thursday', 
+  'Friday', 
+  'Saturday', 
+  'Sunday'
+];
+const MONTH_LABELS = [
+  'January', 
+  'February', 
+  'March', 
+  'April', 
+  'May', 
+  'June', 
+  'July', 
+  'August', 
+  'September', 
+  'October', 
+  'November', 
+  'December'
+];
+
+class Calendar extends Component {
+  constructor(props) {
+    super(props);
+    // Get initial startDate and endDate
+    this.state = {
+      startDate: props.startDate,
+      endDate: props.endDate
+    };
+  }
+
+  onChange = (startDate, endDate) => this.setState({ startDate, endDate });
+
+  render = () => {
+    const { startDate, endDate } = this.state;
+
+    return (
+      <ReactLightCalendar
+        dayLabels={DAY_LABELS}
+        monthLabels={MONTH_LABELS}
+        onChange={this.onChange}
+        startDate={startDate}
+        endDate={endDate}
+        {...this.props} 
+         range displayTime // Add parent's additionnal props
+      />
+    );
+  };
+}
+
+export default Calendar;
